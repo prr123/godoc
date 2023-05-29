4a3db157f0d2b68dd6c50bc535644b6e7d1537ec
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-05-29 10:13:20 +0200

    start

--- creDocList.go
+++ creDocList.go
@@ -10,8 +10,26 @@ package main
 import (
 	"os"
 	"fmt"
+	"log"
 )
 
 func main() {
+
+	numArgs := len(os.Args)
+
+	useStr:= "creDirList [-help]"
+	if numArgs > 2 {
+		fmt.Printf("usage: %s\n", useStr)
+		log.Fatalf("too many args!")
+	}
+
+	if numArgs == 2 {
+		if os.Args[1]== "-help" {
+			fmt.Printf("usage: %s\n", useStr)
+			os.Exit(1)
+		}
+	}
+
+	log.Printf("creDocList start\n")
 
 }
