a3be90d69f53c394cbbc455033d8c55290ca6d72
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-05-29 10:06:55 +0200

    initial commit

---
+++ creDocList.go
@@ -0,0 +1,17 @@
+// creDoc.go
+// program that creates an md file from
+// author: prr azul software
+// date: 29 May 2023
+// copyright 2023 prr, azul software
+//
+
+package main
+
+import (
+	"os"
+	"fmt"
+)
+
+func main() {
+
+}
