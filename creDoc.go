ea87269bf657e2c2f59a8fcd941f976157bbff97
Peter Riemenschneider <peter@vmi1199568.contaboserver.net>
2023-05-29 15:16:06 +0200

    implemented initial steps file checking

--- creDoc.go
+++ creDoc.go
@@ -11,16 +11,23 @@ import (
 	"os"
 	"fmt"
 	"log"
+	"bytes"
 )
 
 func main() {
 
+	var inFilnam string
 	numArgs := len(os.Args)
 
-	useStr:= "creDirList [-help]"
+	useStr:= "creDoc [-help/ file.go]"
 	if numArgs > 2 {
 		fmt.Printf("usage: %s\n", useStr)
 		log.Fatalf("too many args!")
+	}
+
+	if numArgs < 2 {
+		fmt.Printf("usage: %s\n", useStr)
+		log.Fatalf("insufficient args!")
 	}
 
 	if numArgs == 2 {
@@ -28,8 +35,23 @@ func main() {
 			fmt.Printf("usage: %s\n", useStr)
 			os.Exit(1)
 		}
+		inFilnam = os.Args[1]
 	}
 
-	log.Printf("creDocList start\n")
+	// check file name
+	inFilByt := []byte(inFilnam)
+	idx := bytes.Index(inFilByt, []byte(".go"))
+	if idx == -1 {
+		fmt.Printf("usage: %s\n", useStr)
+		log.Fatalf("cli file %s not a go file!", inFilnam)
+	}
 
+
+	data, err := os.ReadFile(inFilnam)
+	if err != nil {log.Fatalf("os.ReadFile: %v", err)}
+	log.Printf("read file: %s size: %d\n", inFilnam, len(data))
+
+	log.Printf("creDoc start parsing\n")
+
+	log.Printf("creDoc end parsing\n")
 }
